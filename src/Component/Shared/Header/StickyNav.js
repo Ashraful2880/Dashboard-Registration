e370103a6f9ba99802b270403eeec4c91c7517c0
Ashraful2880 <ashrafulislam2880@gmail.com>
2022-06-07 13:22:46 +0600

    Top Header Design Done

--- src/Component/Shared/Header/StickyNav.js
+++ src/Component/Shared/Header/StickyNav.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const StickyNav = () => {
     return (
-        <div className="border-b-2">
+        <div className="border-b-2 ">
+            {/*  <ul className=" flex items-center gap-x-5">
+                <li><Link to="/home">Home</Link></li>
+                <li><Link to="/home">About</Link></li>
+                <li><Link to="/home">Service</Link></li>
+                <li><Link to="/home">Contact</Link></li>
+                <li><Link to="/home">Signup</Link></li>
+                <li><Link to="/home">Login</Link></li>
+                <li><Link to="/home">Dashboard</Link></li>
+                <li><Link to="/home">
+                    <img src="https://avatars.githubusercontent.com/u/86690202?v=4" alt="User Img" className="h-10 w-10 rounded-full" />
+                </Link></li>
+            </ul> */}
             This is Sticky Nav
         </div>
     );
--- src/Component/Shared/Header/TopNav.js
+++ src/Component/Shared/Header/TopNav.js
@@ -1,9 +1,38 @@
+import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const TopNav = () => {
     return (
-        <div>
-            <h1 className="text-red-600 text-3xl font-bold"> This is Top Navigation</h1>
+        <div className="bg-gray-900 py-5">
+            <div className="container mx-auto">
+                <div className="flex justify-between items-center">
+                    {/* Left Social Menu Area */}
+                    <div className="flex gap-x-5 items-center text-white">
+                        <div className="flex gap-x-2">
+                            <FontAwesomeIcon icon={faPhone} className="w-5 h-5 animate-bounce" />
+                            <p>Call us at: +880123456789</p>
+                        </div>
+                        <FontAwesomeIcon icon={faFacebook} className="w-5 h-5" />
+                        <FontAwesomeIcon icon={faTwitter} className="w-5 h-5" />
+                        <FontAwesomeIcon icon={faInstagram} className="w-5 h-5" />
+                    </div>
+                    {/* Right Menu Area */}
+                    <div className="text-white flex items-center gap-x-5">
+                        <Link to="/login" className="px-5 py-2 border border-orange-500 hover:bg-orange-600 duration-200 rounded-md">
+                            Login
+                        </Link>
+                        <Link to="/register" className="px-5 py-2 border border-orange-500 hover:bg-orange-600 duration-200 rounded-md">
+                            Registration
+                        </Link>
+                        <Link to="/login" className="bg-orange-600 px-5 py-2 border border-orange-500 hover:bg-transparent duration-200 rounded-md">
+                            Download Apps
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
