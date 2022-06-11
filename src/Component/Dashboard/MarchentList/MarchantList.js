23f7febf4940341659a53d955015ae14e80cd7cb
Ashraful2880 <ashrafulislam2880@gmail.com>
2022-06-11 22:30:37 +0600

    Marchant List Page Design Done

--- src/Component/Dashboard/MarchentList/MarchantList.js
+++ src/Component/Dashboard/MarchentList/MarchantList.js
@@ -6,40 +6,40 @@ const MarchantList = () => {
     const [marchants, setMarchants] = useState([]);
 
     useEffect(() => {
-        fetch(`https://raw.githubusercontent.com/Ashraful2880/FakeData-MarchantList/main/marchantList?token=GHSAT0AAAAAABTFSSV7PHH6CFGASFJM3NEUYVEXKMA`)
+        fetch(`https://raw.githubusercontent.com/Ashraful2880/FakeData-MarchantList/main/marchantList?token=GHSAT0AAAAAABTFSSV6PJOC5JIMA47F34GMYVEYG7Q`)
             .then(res => res.json())
             .then(data => setMarchants(data))
     }, [])
 
     return (
         <div className="container mx-auto">
-            <h3 className="text-xl font-bold">Marchant List</h3>
-            <table className="w-full mx-auto cursor-pointer table">
+            <h3 className="text-2xl font-bold mb-6">Marchant List</h3>
+            <table className="w-full mx-auto cursor-pointer overflow-x-scroll">
                 <thead>
-                    <tr className="text-center text-white py-2">
-                        <th className="bg-black border-x-8 border-white rounded-lg text-center py-2 px-4">#</th>
-                        <th className="bg-black border-x-8 border-white rounded-lg text-center py-2 px-4">Name</th>
-                        <th className="bg-black border-x-8 border-white rounded-lg text-center py-2 px-4">Address</th>
-                        <th className="bg-black border-x-8 border-white rounded-lg text-center py-2 px-4">Email</th>
-                        <th className="bg-black border-x-8 border-white rounded-lg text-center py-2 px-4">Contact</th>
+                    <tr className="text-center text-white">
+                        <th className="bg-black border-x-8 border-white rounded-lg py-3 px-4">#</th>
+                        <th className="bg-black border-x-8 border-white rounded-lg py-3 px-4">Name</th>
+                        <th className="bg-black border-x-8 border-white rounded-lg py-3 px-4">Address</th>
+                        <th className="bg-black border-x-8 border-white rounded-lg py-3 px-4">Email</th>
+                        <th className="bg-black border-x-8 border-white rounded-lg py-3 px-4">Contact</th>
                     </tr>
                 </thead>
                 <tbody>
                     {marchants.map((marchant) => (
-                        <tr className="text-center py-2 border border-green-300 hover:bg-green-800 hover:text-white duration-300" key={marchant.sl}>
-                            <td className="border border-green-300 text-center py-2 px-4">
+                        <tr className="text-center border shadow-xl hover:shadow duration-300" key={marchant.sl}>
+                            <td className="border border-gray-300 py-3 px-4">
                                 {marchant?.sl}
                             </td>
-                            <td className="border border-green-300 text-center py-2 px-4">
+                            <td className="border border-gray-300 py-3 px-4">
                                 {marchant?.name}
                             </td>
-                            <td className="border border-green-300 text-center py-2 px-4">
+                            <td className="border border-gray-300 py-3 px-4">
                                 {marchant?.address}
                             </td>
-                            <td className="border border-green-300 text-center py-2 px-4">
+                            <td className="border border-gray-300 py-3 px-4">
                                 {marchant?.email}
                             </td>
-                            <td className="border border-green-300 text-center py-2 px-4">
+                            <td className="border border-gray-300 py-3 px-4">
                                 {marchant?.contact}
                             </td>
                         </tr>
