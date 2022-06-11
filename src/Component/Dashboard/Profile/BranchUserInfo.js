9c8db0b4474f4cc9c1e3dd3bd5a279224b50c1be
Ashraful2880 <ashrafulislam2880@gmail.com>
2022-06-11 13:16:38 +0600

    Dashboard Profile Page Initialized

---
+++ src/Component/Dashboard/Profile/BranchUserInfo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BranchUserInfo = () => {
+    return (
+        <div className="container mx-auto">
+            This is BranchUserInfo Page
+        </div>
+    );
+};
+
+export default BranchUserInfo;
--- src/Component/Dashboard/Profile/Profile.js
+++ src/Component/Dashboard/Profile/Profile.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { useState } from 'react';
+import BranchUserInfo from './BranchUserInfo';
+import MarchantList from '../MarchentList/MarchantList';
+import RiderList from '../RiderList/RiderList';
 
 const Profile = () => {
+    const [active, setActive] = useState("Branch-User");
+
     return (
-        <div>
-            This is Profile Page
+        <div className="container mx-auto">
+            <h2 className="text-left text-gray-600 text-2xl font-bold lg:mb-0 mb-4 lg:ml-0 ml-5">Profile</h2>
+            <div className="grid lg:grid-cols-3 grid-cols-2 gap-x-6 mb-10 w-1/3 mx-auto">
+                <button
+                    onClick={() => setActive("Branch-User")}
+                    className={`font-semibold uppercase bg-[#F2F2F2] p-2 rounded-3xl mt-2 cursor-pointer hover:bg-green-700 hover:text-white ${active === "Branch-User" && `bg-green-700 text-white`} w-full block shadow-xl border border-green-500 duration-500`}>
+                    Branch User
+                </button>
+                <button
+                    onClick={() => setActive("Marchant-List")}
+                    className={`font-semibold uppercase bg-[#F2F2F2] p-2 rounded-3xl mt-2 cursor-pointer hover:bg-green-700 hover:text-white ${active === "Marchant-List" && `bg-green-700 text-white`} w-full block shadow-xl  border border-green-500 duration-500`}>
+                    Marchant List
+                </button>
+                <button
+                    onClick={() => setActive("Riders List")}
+                    className={`font-semibold uppercase bg-[#F2F2F2] p-2 rounded-3xl mt-2 cursor-pointer hover:bg-green-700 hover:text-white ${active === "Riders List" && `bg-green-700 text-white`} w-full block shadow-xl border border-green-500 duration-500`}>
+                    Riders List
+                </button>
+            </div>
+            <div>
+                {active === "Branch-User" && <BranchUserInfo />}
+                {active === "Marchant-List" && <MarchantList />}
+                {active === "Riders List" && <RiderList />}
+            </div>
         </div>
     );
 };
