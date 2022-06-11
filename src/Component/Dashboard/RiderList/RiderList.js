31035eebb672560c7aff0e8d0fb2bf5a700f8f7f
Ashraful2880 <ashrafulislam2880@gmail.com>
2022-06-11 22:48:09 +0600

    Riders List Page Structure Added

--- src/Component/Dashboard/RiderList/RiderList.js
+++ src/Component/Dashboard/RiderList/RiderList.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const RiderList = () => {
+    const [riders, setRiders] = useState([]);
+
+    useEffect(() => {
+        fetch("./RiderListData.json")
+            .then(res => res.json())
+            .then(data => setRiders(data))
+    }, [])
+
+    console.log(riders);
     return (
         <div className="container mx-auto">
-            <h3 className="text-xl font-bold">Rider List</h3>
+            <h3 className="text-2xl font-bold mb-6">Rider List</h3>
+            <table className="w-full mx-auto cursor-pointer overflow-x-scroll">
+                <thead>
+                    <tr className="text-center text-white">
+                        <th className="bg-black border-x-8 border-white rounded-lg py-3 px-4">#</th>
+                        <th className="bg-black border-x-8 border-white rounded-lg py-3 px-4">Name</th>
+                        <th className="bg-black border-x-8 border-white rounded-lg py-3 px-4">Address</th>
+                        <th className="bg-black border-x-8 border-white rounded-lg py-3 px-4">Contact</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {riders.map((rider) => (
+                        <tr className="text-center border shadow-xl hover:shadow duration-300" key={rider.sl}>
+                            <td className="border border-gray-300 py-3 px-4">
+                                {rider?.sl}
+                            </td>
+                            <td className="border border-gray-300 py-3 px-4">
+                                {rider?.name}
+                            </td>
+                            <td className="border border-gray-300 py-3 px-4">
+                                {rider?.address}
+                            </td>
+                            <td className="border border-gray-300 py-3 px-4">
+                                {rider?.contact}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 };
