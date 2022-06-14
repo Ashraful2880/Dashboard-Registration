5722bdab1773e10a576b14a3305cd6a41daf9d5d
Ashraful2880 <ashrafulislam2880@gmail.com>
2022-06-14 12:09:04 +0600

    Generate Pickup Rider Run Page Design Added

--- src/Component/Dashboard/Pickup-Parcel/GeneratePickupRiderRun.js
+++ src/Component/Dashboard/Pickup-Parcel/GeneratePickupRiderRun.js
@@ -2,8 +2,62 @@ import React from 'react';
 
 const GeneratePickupRiderRun = () => {
     return (
-        <div>
-            This is GeneratePickupRiderRun page
+        <div className="px-10 mx-auto">
+            <h3 className="text-2xl font-bold mb-6 text-left text-gray-500">Generate Pickup Rider Run</h3>
+            <div className="lg:flex block justify-between w-full">
+                <form className="border border-gray-200 py-5 rounded-md shadow-md lg:w-1/2 w-full lg:px-10 px-6 lg:mx-6 mx-0">
+                    <h2 className="text-green-700 text-xl font-bold text-left lg:mb-10 mb-2">Pickup Run</h2>
+                    <div className="flex items-center justify-between py-2 px-5 my-2 border rounded-md">
+                        <p className="font-semibold">Rider</p>
+                        <select
+                            name="Entries"
+                            className="border border-gray-300 focus:outline-none rounded-md px-2 py-1 mx-2 w-1/2">
+                            <option selected>Select Rider</option>
+                            <option defaultValue="Omar Faruk">Omar Faruk</option>
+                            <option defaultValue="Mr. Tahmidur Rahman">Mr. Tahmidur Rahman</option>
+                            <option defaultValue="Istiyaq Hossain Chowdhury">Istiyaq Hossain Chowdhury</option>
+                            <option defaultValue="Md.Ibrahim Khalil Ullah">Md.Ibrahim Khalil Ullah</option>
+                            <option defaultValue="Tafim Hossain">Tafim Hossain</option>
+                        </select>
+                    </div>
+                    <div className="flex items-center justify-between py-2 px-5 my-2 border rounded-md">
+                        <p className="font-semibold">Date</p>
+                        <input type="date" name="date" />
+                    </div>
+                    <div className="flex items-center justify-between py-2 px-5 my-2 border rounded-md">
+                        <p className="font-semibold">Rider Name</p>
+                        <p>Md. Tahmidur Rahman</p>
+                    </div>
+                    <div className="flex items-center justify-between py-2 px-5 my-2 border rounded-md">
+                        <p className="font-semibold">Rider Contact Number</p>
+                        <p>01810007652</p>
+                    </div>
+                    <div className="flex items-center justify-between py-2 px-5 my-2 border rounded-md">
+                        <p className="font-semibold">Rider Address</p>
+                        <p>Mirpur</p>
+                    </div>
+                    <div className="flex items-center justify-between py-2 px-5 my-2 border rounded-md">
+                        <p className="font-semibold">Total Run Parcel</p>
+                        <p>0</p>
+                    </div>
+                    <textarea className="border w-full px-5 focus:outline-none resize-none" name="run note" cols="30" rows="4" placeholder="Pickup rider Run Note"></textarea>
+                    <div>
+                        <input className="text-lg border rounded-md mt-4 border-green-700 mx-4 py-1.5 px-5 bg-green-700 text-white hover:text-green-800 hover:bg-transparent duration-300" type="submit" value="Generate" />
+                        <input className="text-lg border rounded-md mt-4 border-red-600 mx-4 py-1.5 px-5 bg-red-600 text-white hover:text-red-600 hover:bg-transparent duration-300" type="reset" value="Reset" />
+                    </div>
+                </form>
+                <div className="lg:w-1/2 w-full border border-gray-200 py-5 rounded-md shadow-md lg:px-10 px-6 lg:mx-6 mx-0">
+                    <form>
+                        <div>
+                            <input type="text" name="text" className="border rounded-md w-full py-2 px-5 my-2 focus:outline-none focus:border-green-600" placeholder="Enter Parcel Invoice Barcode" />
+                        </div>
+                        <div>
+                            <input type="text" name="text" className="border rounded-md w-full py-2 px-5 my-2 focus:outline-none focus:border-green-600" placeholder="Enter Marchant Order ID" />
+                        </div>
+                        <input className="bg-green-700 border border-green-700 hover:bg-transparent text-white hover:text-green-700 duration-500 py-1.5 px-5 rounded-md w-full" type="submit" value="Search" />
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
