9239c182badcfb8e6cafa329311ca012e24ca155
Ashraful2880 <ashrafulislam2880@gmail.com>
2022-06-15 15:17:07 +0600

    Return Parcel List Page Design Done

--- src/Component/Dashboard/Return-Parcel/ReturnParcelList.js
+++ src/Component/Dashboard/Return-Parcel/ReturnParcelList.js
@@ -1,9 +1,148 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPen, faPrint, faEye, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 
 const ReturnParcelList = () => {
+    const [showData, setShowData] = useState(10);
+    const [returnParcelLists, setReturnParcelLists] = useState([]);
+
+    useEffect(() => {
+        fetch("/pickUpParcelList.json")
+            .then(res => res.json())
+            .then(data => setReturnParcelLists(data))
+    }, [])
+
     return (
-        <div>
-            This is ReturnParcelList page
+        <div className="px-10 mx-auto">
+            <h3 className="text-2xl font-bold mb-6 text-left">Return Parcel List</h3>
+            <div className="border border-gray-200 py-5 rounded-md shadow-xl">
+                {/* Generate Return Branch Transfer Section Design Here */}
+                <div className="flex justify-between items-center mx-10 mb-5">
+                    <h3 className="font-bold text-lg lg:block hidden">Return Parcels List</h3>
+                    <div>
+                        <Link to="/dashboard/generateReturnRiderRun" className="bg-green-800 border border-green-800 text-white py-2 px-4 rounded-md hover:bg-transparent hover:text-green-800 duration-300 font-semibold">
+                            <FontAwesomeIcon icon={faPen} className="mx-2" />
+                            Generate Return Rider Run
+                        </Link>
+                    </div>
+                </div>
+                {/* Top Pagination and Search Area Design Here */}
+                <div className="lg:flex block justify-between items-center my-2 mx-10">
+                    <div>
+                        Show <span>
+                            <select
+                                onChange={(e) => setShowData(e.target.value)}
+                                name="Entries"
+                                className="border border-gray-300 focus:outline-none focus:border focus:border-green-600 rounded-md px-2 py-1 mx-2">
+                                <option selected>10</option>
+                                <option defaultValue="25">25</option>
+                                <option defaultValue="50">50</option>
+                                <option defaultValue="100">100</option>
+                            </select>
+                        </span> Entries
+                    </div>
+                    <div className="lg:mt-0 mt-2">
+                        <label htmlFor="text" className="font-semibold mr-2 lg:inline-block hidden">Search </label>
+                        <input
+                            type="search"
+                            placeholder="Search Here"
+                            className="border px-5 py-2 rounded-md focus:outline-0 focus:border focus:border-green-600 duration-300 border-gray-300" />
+                    </div>
+                </div>
+                {/* Main Table Area Here */}
+                <div className="my-2 overflow-x-auto sm:-mx-6 lg:mx-0">
+                    <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div className="shadow overflow-hidden rounded-lg">
+                            <table className="min-w-full divide-y divide-x divide-gray-200">
+                                <thead className="bg-green-800 text-white text-center text-xs font-semibold uppercase">
+                                    <tr>
+                                        <th className="px-6 py-3 tracking-wider border">
+                                            SL
+                                        </th>
+                                        <th className="px-6 py-3 tracking-wider border">
+                                            Invoice
+                                        </th>
+                                        <th className="px-6 py-3 tracking-wider border">
+                                            Merchant Name
+                                        </th>
+                                        <th className="px-6 py-3 tracking-wider border">
+                                            Merchant Address
+                                        </th>
+                                        <th className="px-6 py-3 tracking-wider border">
+                                            Upazilla
+                                        </th>
+                                        <th className="px-6 py-3 tracking-wider border">
+                                            Area
+                                        </th>
+                                        <th className="px-6 py-3 tracking-wider border">
+                                            Charge
+                                        </th>
+                                        <th className="px-6 py-3 uppercase tracking-wider border">
+                                            Status
+                                        </th>
+                                        <th className="px-6 py-3 uppercase tracking-wider border">
+                                            Action
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody className="bg-white divide-y divide-x divide-gray-200 text-gray-900 text-center text-sm font-normal">
+                                    {returnParcelLists.slice(0, showData).map((deliveryParcel) => (
+                                        <tr key={deliveryParcel.sl} className="hover:bg-gray-100 duration-200">
+                                            <td className="px-2 py-3 border">
+                                                {deliveryParcel?.sl}
+                                            </td>
+                                            <td className="px-2 py-3 border">
+                                                {deliveryParcel?.invoice}
+                                            </td>
+                                            <td className="px-2 py-3 border">
+                                                {deliveryParcel?.marchantName}
+                                            </td>
+                                            <td className="px-2 py-3 border">
+                                                {deliveryParcel?.address}
+                                            </td>
+                                            <td className="px-2 py-3 border">
+                                                {deliveryParcel?.district}
+                                            </td>
+                                            <td className="px-2 py-3 border">
+                                                {deliveryParcel?.district}
+                                            </td>
+                                            <td className="px-2 py-3 border">
+                                                {deliveryParcel?.charge}
+                                            </td>
+                                            <td className="px-2 py-3 border">
+                                                Pending
+                                            </td>
+                                            <td className="px-2 py-3 border">
+                                                <div className="flex justify-evenly items-center">
+                                                    <button>
+                                                        <FontAwesomeIcon icon={faPrint} className="h-4 w-4 bg-green-600 text-white px-2 py-1 rounded-sm" />
+                                                    </button>
+                                                    <button >
+                                                        <FontAwesomeIcon icon={faEye} className="h-4 w-4 bg-gray-600 text-white px-2 py-1 rounded-sm" />
+                                                    </button>
+                                                    <button>
+                                                        <FontAwesomeIcon icon={faPenToSquare} className="h-4 w-4 bg-cyan-600 text-white px-2 py-1 rounded-sm" />
+                                                    </button>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div className="lg:flex block justify-between items-center my-2 mx-10">
+                    <p>Showing <span className="font-semibold">1</span> to <span className="font-semibold">{returnParcelLists.slice(0, showData).length}</span> of <span className="font-semibold">{returnParcelLists?.length}</span> Entries</p>
+                    <div>
+                        pagination Here
+                    </div>
+                </div>
+            </div>
+            {/* <div>
+                <ViewDetailsModal display={display} setDisplay={setDisplay} query={query} />
+            </div> */}
         </div>
     );
 };
