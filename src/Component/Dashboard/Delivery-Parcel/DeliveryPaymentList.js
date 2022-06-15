4776efd07ea69abb0919d1d1acbe2e4df22a58fb
Ashraful2880 <ashrafulislam2880@gmail.com>
2022-06-15 12:49:14 +0600

    Delivery Payment List Page Design Complete

--- src/Component/Dashboard/Delivery-Parcel/DeliveryPaymentList.js
+++ src/Component/Dashboard/Delivery-Parcel/DeliveryPaymentList.js
@@ -1,9 +1,169 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPen, faSearch, faRefresh, faEye, faPrint, faCheck } from '@fortawesome/free-solid-svg-icons';
 
 const DeliveryPaymentList = () => {
+    const [showData, setShowData] = useState(10);
+    const [deliveryPaymentLista, setDeliveryPaymentLists] = useState([]);
+
+    useEffect(() => {
+        fetch("/pickUpParcelList.json")
+            .then(res => res.json())
+            .then(data => setDeliveryPaymentLists(data))
+    }, [])
+
     return (
-        <div>
-            This is DeliveryPaymentList page
+        <div className="px-10 mx-auto">
+            <h3 className="text-2xl font-bold mb-6 text-left">Delivery Payment List</h3>
+            <div className="border border-gray-200 pb-5 rounded-md shadow-xl">
+                {/* Import And Generate Section Design Here */}
+                <div className="lg:flex block justify-between items-center border-b py-3 px-10">
+                    <h4 className="text-md font-bold">Delivery Payment List</h4>
+                    <div className="lg:my-0 my-2">
+                        <Link to="/dashboard/deliveryPaymentGenerate" className="bg-green-700 hover:bg-green-800 duration-300 py-2 px-4 rounded-md text-white text-md font-semibold mx-2">
+                            <FontAwesomeIcon icon={faPen} className="text-white mr-1" /> Generate Delivery Payment
+                        </Link>
+                    </div>
+                </div>
+
+                {/* Riders Select Options Here */}
+
+                <div className="grid lg:grid-cols-5 md:grid-cols-2 grid-cols-1 gap-x-6 px-10 border-b pb-4 w-full">
+                    <div>
+                        <h5 className="text-md font-semibold text-left my-2">Delivery Payment Type:</h5>
+                        <select
+                            onChange={(e) => setShowData(e.target.value)}
+                            name="Entries"
+                            className="border border-gray-300 focus:outline-none focus:border focus:border-green-600 rounded-md px-2 py-1 w-full">
+                            <option selected>Select Delivery Payment Type</option>
+                            <option defaultValue="Send Request">Send Request</option>
+                            <option defaultValue="Request Accept">Request Accept</option>
+                            <option defaultValue="Request Cancel">Request Cancel</option>
+                        </select>
+                    </div>
+                    <div>
+                        <h5 className="text-md font-semibold text-left my-2">From Date:</h5>
+                        <input type="date" name="date" className="border border-gray-300 px-5 py-1 rounded-md w-full" />
+                    </div>
+                    <div>
+                        <h5 className="text-md font-semibold text-left my-2">To Date:</h5>
+                        <input type="date" name="date" className="border border-gray-300 px-5 py-1 rounded-md w-full" />
+                    </div>
+                    <div className="flex items-end justify-end w-full lg:my-0 my-2">
+                        <button className="bg-green-700 hover:bg-green-900 duration-300 px-5 py-1 rounded-md mx-1">
+                            <FontAwesomeIcon icon={faSearch} className="text-white" />
+                        </button>
+                        <button className="bg-red-600 hover:bg-red-700 duration-300 px-5 py-1 rounded-md mx-">
+                            <FontAwesomeIcon icon={faRefresh} className="text-white" />
+                        </button>
+                    </div>
+                </div>
+                {/* Table Search And PAgination Area Here */}
+                <div className="lg:flex block justify-between items-center my-2 mx-10">
+                    <div>
+                        Show <span>
+                            <select
+                                onChange={(e) => setShowData(e.target.value)}
+                                name="Entries"
+                                className="border border-gray-300 focus:outline-none focus:border focus:border-green-600 rounded-md px-2 py-1 mx-2">
+                                <option selected>10</option>
+                                <option defaultValue="25">25</option>
+                                <option defaultValue="50">50</option>
+                                <option defaultValue="100">100</option>
+                            </select>
+                        </span> Entries
+                    </div>
+                    <div className="lg:mt-0 mt-2">
+                        <label htmlFor="text" className="font-semibold mr-2 lg:inline-block hidden">Search </label>
+                        <input
+                            type="search"
+                            placeholder="Search Here"
+                            className="border px-5 py-2 rounded-md focus:outline-0 focus:border focus:border-green-600 duration-300 border-gray-300" />
+                    </div>
+                </div>
+                {/* Main Table Design Here */}
+                <div className="my-2 overflow-x-auto sm:-mx-6 lg:mx-0">
+                    <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div className="shadow overflow-hidden rounded-lg">
+                            <table className="min-w-full divide-y divide-x divide-gray-200">
+                                <thead className="bg-green-800 text-white text-center  text-xs font-semibold uppercase">
+                                    <tr>
+                                        <th className="px-6 py-3 tracking-wider border">
+                                            SL
+                                        </th>
+                                        <th className="px-6 py-3 tracking-wider border">
+                                            Consignment
+                                        </th>
+                                        <th className="px-6 py-3 tracking-wider border">
+                                            Admin
+                                        </th>
+                                        <th className="px-6 py-3 tracking-wider border">
+                                            Payment Parcel
+                                        </th>
+                                        <th className="px-6 py-3 tracking-wider border">
+                                            Received Payment Parcel
+                                        </th>
+                                        <th className="px-6 py-3 tracking-wider border">
+                                            Payment Amount
+                                        </th>
+                                        <th className="px-6 py-3 tracking-wider border">
+                                            Received Payment Amount
+                                        </th>
+                                        <th className="px-6 py-3 tracking-wider border">
+                                            Status
+                                        </th>
+                                        <th className="px-6 py-3 uppercase tracking-wider border">
+                                            Action
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody className="bg-white divide-y divide-x divide-gray-200 text-gray-900 text-center text-sm font-normal">
+                                    {deliveryPaymentLista.slice(0, showData).map((parcel) => (
+                                        <tr key={parcel.sl} className="hover:bg-gray-100 duration-200">
+                                            <td className="px-2 py-3 border">
+                                                {parcel?.sl}
+                                            </td>
+                                            <td className="px-2 py-3 border">
+                                                {parcel?.invoice}
+                                            </td>
+                                            <td className="px-2 py-3 border">
+                                                {parcel?.marchantName}
+                                            </td>
+                                            <td className="px-2 py-3 border">
+                                                60
+                                            </td>
+                                            <td className="px-2 py-3 border">
+                                                60
+                                            </td>
+                                            <td className="px-2 py-3 border">
+                                                70
+                                            </td>
+                                            <td className="px-2 py-3 border">
+                                                100
+                                            </td>
+                                            <td className="px-2 py-3 border">
+                                                Pending
+                                            </td>
+                                            <td className="px-2 py-3 border">
+                                                <button >
+                                                    <FontAwesomeIcon icon={faEye} className="h-4 w-4 bg-gray-600 text-white px-2 py-1 rounded-sm" />
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div className="lg:flex block justify-between items-center my-2 mx-10">
+                    <p className="border border-gray-300 px-4 py-1 rounded-md">Showing <span className="font-semibold">1</span> to <span className="font-semibold">{deliveryPaymentLista.slice(0, showData).length}</span> of <span className="font-semibold">{deliveryPaymentLista?.length}</span> Entries</p>
+                    <div>
+                        pagination Here
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
